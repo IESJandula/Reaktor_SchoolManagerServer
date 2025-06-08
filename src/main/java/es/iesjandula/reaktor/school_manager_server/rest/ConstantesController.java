f5399e917346caa2d77aff5aca62d6dfb6ec7843
Virginia <virgi.mor.98@gmail.com>
2025-06-08 19:50:03 +0200

    Refactorizado manejo de mensajes de error en `ConstantesController` para reutilizar variables, normalización del estado HTTP utilizando `HttpStatus.INTERNAL_SERVER_ERROR`, y mejoras en mensajes de log.

--- src/main/java/es/iesjandula/reaktor/school_manager_server/rest/ConstantesController.java
+++ src/main/java/es/iesjandula/reaktor/school_manager_server/rest/ConstantesController.java
@@ -8,6 +8,7 @@ import es.iesjandula.reaktor.school_manager_server.utils.Constants;
 import es.iesjandula.reaktor.school_manager_server.utils.SchoolManagerServerException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,12 +38,12 @@ public class ConstantesController
 		}
 		catch (Exception exception)
 		{
-
+			String mensajeError = "Excepción genérica al obtener las costantes";
 			SchoolManagerServerException schoolManagerServerException = new SchoolManagerServerException(Constants.CONSTANTE_NO_ENCONTRADA,
-					"Excepción genérica al obtener las costantes", exception);
+					mensajeError, exception);
 
-			log.error("Excepción genérica al obtener las costantes", schoolManagerServerException);
-			return ResponseEntity.status(500).body(schoolManagerServerException.getBodyExceptionMessage());
+			log.error(mensajeError, schoolManagerServerException);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(schoolManagerServerException.getBodyExceptionMessage());
 		}
 	}
 
@@ -63,11 +64,12 @@ public class ConstantesController
 		}
 		catch (Exception exception)
 		{
+			String mensajeError = "Excepción genérica al obtener las costantes";
 			SchoolManagerServerException schoolManagerServerException = new SchoolManagerServerException(Constants.CONSTANTE_NO_ENCONTRADA,
-					"Excepción genérica al obtener las costantes", exception);
+					mensajeError, exception);
 
-			log.error("Excepción genérica al actualizar las costantes");
-			return ResponseEntity.status(500).body(schoolManagerServerException.getBodyExceptionMessage());
+			log.error(mensajeError);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(schoolManagerServerException.getBodyExceptionMessage());
 		}
 	}
 }
